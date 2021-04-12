97cac5713fde695b7b1326bc18ea82c4e37a1b14
Carmona <Carmona@DESKTOP-6V0L5V4.lan>
2021-04-12 23:31:59 +0200

    HOLA

---
+++ pronombre1nombre2/src/pronombre1nombre2/proyecto.java
@@ -0,0 +1,16 @@
+package pronombre1nombre2;
+
+public class proyecto {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("proyectos");
+		
+		
+	}
+
+}
