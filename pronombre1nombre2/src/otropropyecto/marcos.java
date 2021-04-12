a613a4084af9fbcd367bfad0eb22213a82a47dc6
Carmona <Carmona@DESKTOP-6V0L5V4.lan>
2021-04-13 00:32:57 +0200

    Hola6

---
+++ pronombre1nombre2/src/otropropyecto/marcos.java
@@ -0,0 +1,16 @@
+package otropropyecto;
+
+public class marcos {
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("proyecto6");
+		
+	}
+
+}
